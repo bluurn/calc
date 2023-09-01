ba3cd22e037cfad9936f7134c852acf3a69a419d
vladimir.suvorov <vladimir.suvorov@xempus.com>
2023-09-01 14:42:42 +0200

    add more tests lol

--- test.cpp
+++ test.cpp
@@ -5,8 +5,22 @@
 #include "parser.h"
 
 TEST_CASE("a test case for token stream") {
-  std::string expr{"2+2;"};
-  std::istringstream iss(expr);
-  TokenStream ts(iss);
-  REQUIRE( expression(ts) == 4 );
+  {
+    std::string expr{"2+2;"};
+    std::istringstream iss(expr);
+    TokenStream ts(iss);
+    REQUIRE( expression(ts) == 4 );
+  }
+  {
+    std::string expr{"2+2*2+2;"};
+    std::istringstream iss(expr);
+    TokenStream ts(iss);
+    REQUIRE( expression(ts) == 8 );
+  }
+  {
+    std::string expr{"2;"};
+    std::istringstream iss(expr);
+    TokenStream ts(iss);
+    REQUIRE( expression(ts) == 2 );
+  }
 }
